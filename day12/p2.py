0b252372266a2c5c4de415cc039eff8d7dd1f69c
Katie Siegel <katie@getflipturn.com>
2022-12-12 00:30:47 -0500

    day12

---
+++ day12/p2.py
@@ -0,0 +1,52 @@
+from sys import argv
+
+
+def main(filename):
+    # Read the input file
+    with open(filename, "r") as f:
+        input = [x.strip() for x in f.readlines()]
+
+    grid = []
+    bfs = []
+    for i in range(len(input)):
+        l = input[i]
+        row = []
+        for c in range(len(l)):
+            item = l[c]
+            if item == "S":
+                item = "a"
+            elif item == "E":
+                item = "z"
+                end = [i, c]
+            val = -1
+            if item == "a":
+                val = 0
+                bfs.append([i, c])
+            row.append([item, val])
+        grid.append(row)
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    while len(bfs) > 0:
+        curr = bfs.pop(0)
+        top = [curr[0], curr[1]-1]
+        bottom = [curr[0], curr[1]+1]
+        left = [curr[0]-1, curr[1]]
+        right = [curr[0]+1, curr[1]]
+
+        curr_val = grid[curr[0]][curr[1]][0]
+        for poss in [top, bottom, left, right]:
+            if poss[0] < 0 or poss[0] >= len(grid) or poss[1] < 0 or poss[1] >= len(grid[0]):
+                continue
+            poss_val = grid[poss[0]][poss[1]][0]
+            if alphabet.index(curr_val) + 1 >= alphabet.index(poss_val):
+                path_len = grid[curr[0]][curr[1]][1]+1
+                if grid[poss[0]][poss[1]][1] > path_len or grid[poss[0]][poss[1]][1] == -1:
+                    # can move here, haven't visited yet
+                    grid[poss[0]][poss[1]][1] = path_len
+                    bfs.append(poss)
+
+    print(grid[end[0]][end[1]][1])
+
+
+if __name__ == "__main__":
+    main(argv[1])
